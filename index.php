efb7dad8388c2e49ea8dea74e20db7c8f3270617
Elovard <persall2013@gmail.com>
2021-09-27 23:07:00 +0300

    fix when data doesn't reach db

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-include 'main.php';
+require 'main.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -14,7 +14,8 @@ include 'main.php';
 <div class="container">
     <div class="row">
         <div class="col mt-1">
-            <button class="btn btn-success mb-1" data-toggle="modal" data-target="#Modal"><i class="fa fa-user-plus"></i></button>
+            <button class="btn btn-success mb-1" data-toggle="modal" data-target="#Modal"><i
+                        class="fa fa-user-plus"></i></button>
             <table class="table shadow ">
                 <thead class="thead-dark">
                 <tr>
@@ -24,17 +25,19 @@ include 'main.php';
                     <th>PRICE</th>
                     <th>ACTION</th>
                 </tr>
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td>
-                            <a href="" class="btn btn-success btn-sm" data-toggle="modal" data-target="#editModal"><i class="fa fa-edit"></i></a>
-                            <a href="?delete=" class="btn btn-danger btn-sm" data-toggle="modal" data-target="#deleteModal"><i class="fa fa-trash"></i></a>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td>
+                        <a href="" class="btn btn-success btn-sm" data-toggle="modal" data-target="#editModal"><i
+                                    class="fa fa-edit"></i></a>
+                        <a href="?delete=" class="btn btn-danger btn-sm" data-toggle="modal" data-target="#deleteModal"><i
+                                    class="fa fa-trash"></i></a>
 
-                        </td>
-                    </tr>
+                    </td>
+                </tr>
                 </thead>
             </table>
         </div>
@@ -72,10 +75,13 @@ include 'main.php';
 </div>
 
 
-
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous"></script>
 <script>
     </body>
     </html>
